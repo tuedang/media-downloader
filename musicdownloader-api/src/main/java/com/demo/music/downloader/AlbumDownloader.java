bba88e7446cdbf8b5cbbfe1252c321928aa84aff
Tue Dang <dangtritue@gmail.com>
2016-06-26 02:02:29 +0700

    add Csn support

--- musicdownloader-api/src/main/java/com/demo/music/downloader/AlbumDownloader.java
+++ musicdownloader-api/src/main/java/com/demo/music/downloader/AlbumDownloader.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.OutputStream;
 
 import com.demo.music.generator.AudioTagger;
+import com.demo.parser.common.FileUtils;
 import com.demo.parser.common.StringHtmlUtils;
 import com.demo.music.downloader.TargetOutputStreamContext.TargetType;
 import com.demo.music.downloader.httpdownloader.SimpleHttpDownloader;
@@ -11,10 +12,9 @@ import com.demo.music.generator.M3UGenerator;
 import com.demo.music.sdo.Album;
 import com.demo.music.sdo.TagInfo;
 import com.demo.music.sdo.Track;
-import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.lang3.StringUtils;
 
-public class AlbumDownloader{
+public class AlbumDownloader {
     private Album album;
     private TargetOutputStreamContext targetOutputStreamContext;
 
@@ -22,31 +22,32 @@ public class AlbumDownloader{
         this.album = album;
         this.targetOutputStreamContext = targetContext;
     }
+
     private HttpDownloader httpDownloader = new SimpleHttpDownloader();
 
-    public void downloadAlbum(DownloadCallback downloadCallback) throws IllegalStateException, IOException{
+    public void downloadAlbum(DownloadCallback downloadCallback) throws IllegalStateException, IOException {
 
         //image album
         String artistName = album.getArtist();
-        if(artistName==null || artistName.isEmpty()) {
-            artistName="album";
+        if (artistName == null || artistName.isEmpty()) {
+            artistName = "album";
         }
 
         Status status = new Status();
         status.setStatusType(StatusType.DOWNLOAD_IMAGE);
         downloadCallback.updateStatus(status);
 
-        String imageFileName = artistName+ getExt(album.getImageLink(), "jpg");
-        if(StringUtils.isNotEmpty(album.getImageLink()) && !targetOutputStreamContext.existed(imageFileName)) {
+        String imageFileName = artistName + FileUtils.getExt(album.getImageLink(), "jpg");
+        if (StringUtils.isNotEmpty(album.getImageLink()) && !targetOutputStreamContext.existed(imageFileName)) {
             OutputStream imgOs = targetOutputStreamContext.createOutputStream(imageFileName);
             httpDownloader.download(album.getImageLink(), imgOs);
             imgOs.close();
         }
 
         String noteFileName = "note.txt";
-        if(!targetOutputStreamContext.existed(noteFileName)) {
+        if (!targetOutputStreamContext.existed(noteFileName)) {
             OutputStream note = targetOutputStreamContext.createOutputStream(noteFileName);
-            String noteContent = "Reference link:"+album.getRefLink();
+            String noteContent = "Reference link:" + album.getRefLink();
             note.write(noteContent.getBytes());
             note.close();
         }
@@ -55,21 +56,21 @@ public class AlbumDownloader{
         status.setStatusType(StatusType.DOWNLOAD_SOUNDTRACK);
         status.setTotalTrack(album.getTracks().size());
         downloadCallback.updateStatus(status);
-        for(Track track: album.getTracks()) {
-            String mp3FileName = StringHtmlUtils.unAccent(track.getTitle())+getExt(track.getLocation(), "mp3");
-            if(targetOutputStreamContext.existed(mp3FileName)) {
+        for (Track track : album.getTracks()) {
+            String mp3FileName = StringHtmlUtils.unAccent(track.getTitle()) + FileUtils.getExt(track.getLocation(), "mp3");
+            if (targetOutputStreamContext.existed(mp3FileName)) {
                 System.out.println(String.format("[%s] - Target file has been existed, do not download again.", mp3FileName));
                 continue;
             }
-            OutputStream mp3os= targetOutputStreamContext.createOutputStream(mp3FileName);
+            OutputStream mp3os = targetOutputStreamContext.createOutputStream(mp3FileName);
             httpDownloader.download(track.getLocation(), mp3os);
 
-            String artist = StringUtils.isNotEmpty(track.getCreator()) ? track.getCreator(): album.getArtist();
+            String artist = StringUtils.isNotEmpty(track.getCreator()) ? track.getCreator() : album.getArtist();
             TagInfo tag = new TagInfo(track.getId(), artist, album.getName(), track.getTitle());
             tag.setTotalTrack(album.getTracks().size());
             System.out.println(String.format("Tag file %s=[%s]\n", mp3FileName, tag));
 
-            if(targetOutputStreamContext.getTargetType()==TargetType.FILE_SYSTEM) {
+            if (targetOutputStreamContext.getTargetType() == TargetType.FILE_SYSTEM) {
                 AudioTagger.tag(targetOutputStreamContext.getFileSystem(mp3FileName), tag);
             }
 
@@ -77,8 +78,8 @@ public class AlbumDownloader{
             downloadCallback.updateStatus(status);
         }
 
-        System.out.println("Generate playlist:"+album.getName()+".m3u");
-        OutputStream m3u = targetOutputStreamContext.createOutputStream(StringHtmlUtils.unAccent(album.getName()) +".m3u");
+        System.out.println("Generate playlist:" + album.getName() + ".m3u");
+        OutputStream m3u = targetOutputStreamContext.createOutputStream(StringHtmlUtils.unAccent(album.getName()) + ".m3u");
         M3UGenerator.generateM3U(album, m3u);
         m3u.close();
 
@@ -86,14 +87,6 @@ public class AlbumDownloader{
         System.out.println("Done............................................");
         status.setStatusType(StatusType.FINISH);
         downloadCallback.updateStatus(status);
-    }
-
-    private String getExt(String url, String defaultValue) {
-        String ext = FilenameUtils.getExtension(url);
-        if (ext == null || ext.isEmpty() || ext.length() > 4) {
-            ext = defaultValue;
-        }
-        return "." + ext;
     }
 
 }
