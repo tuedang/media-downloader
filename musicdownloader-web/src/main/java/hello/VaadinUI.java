beca308f3d5111ed4ab8f7aa4fddb204e209536c
Tue Dang <dangtritue@gmail.com>
2016-06-29 23:50:15 +0700

    minor ui change

--- musicdownloader-web/src/main/java/hello/VaadinUI.java
+++ musicdownloader-web/src/main/java/hello/VaadinUI.java
@@ -15,85 +15,87 @@ import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 
+import java.util.concurrent.CompletableFuture;
+
 @SpringUI
 @Theme("valo")
 public class VaadinUI extends UI {
 
-	private final CustomerRepository repo;
+    private final CustomerRepository repo;
 
-	private final CustomerEditor editor;
+    private final CustomerEditor editor;
 
-	private final Grid grid;
+    private final Grid grid;
 
-	private final TextField filter;
+    private final TextField filter;
 
-	private final Button addNewBtn;
+    private final Button downloadButton;
 
-	@Autowired
-	public VaadinUI(CustomerRepository repo, CustomerEditor editor) {
-		this.repo = repo;
-		this.editor = editor;
-		this.grid = new Grid();
-		this.filter = new TextField();
-		this.addNewBtn = new Button("New customer", FontAwesome.PLUS);
-	}
+    @Autowired
+    public VaadinUI(CustomerRepository repo, CustomerEditor editor) {
+        this.repo = repo;
+        this.editor = editor;
+        this.grid = new Grid();
+        this.filter = new TextField();
+        this.downloadButton = new Button("Download", FontAwesome.DOWNLOAD);
+    }
 
-	@Override
-	protected void init(VaadinRequest request) {
-		// build layout
-		HorizontalLayout actions = new HorizontalLayout(filter, addNewBtn);
-		VerticalLayout mainLayout = new VerticalLayout(actions, grid, editor);
-		setContent(mainLayout);
+    @Override
+    protected void init(VaadinRequest request) {
+        // build layout
+        HorizontalLayout actions = new HorizontalLayout(filter, downloadButton);
+        VerticalLayout mainLayout = new VerticalLayout(actions, grid, editor);
+        setContent(mainLayout);
 
-		// Configure layouts and components
-		actions.setSpacing(true);
-		mainLayout.setMargin(true);
-		mainLayout.setSpacing(true);
+        // Configure layouts and components
+        actions.setSpacing(true);
+        mainLayout.setMargin(true);
+        mainLayout.setSpacing(true);
 
-		grid.setHeight(300, Unit.PIXELS);
-		grid.setColumns("id", "firstName", "lastName");
+        grid.setHeight(300, Unit.PIXELS);
+        grid.setWidth(800, Unit.PIXELS);
+        grid.setColumns("id", "firstName", "lastName");
 
-		filter.setInputPrompt("Filter by last name");
+        filter.setInputPrompt("Grap the link from browser and paste here!");
+        filter.setWidth(600, Unit.PIXELS);
 
-		// Hook logic to components
+        // Hook logic to components
 
-		// Replace listing with filtered content when user changes filter
-		filter.addTextChangeListener(e -> listCustomers(e.getText()));
+        // Replace listing with filtered content when user changes filter
+        filter.addTextChangeListener(e -> listCustomers(e.getText()));
 
-		// Connect selected Customer to editor or hide if none is selected
-		grid.addSelectionListener(e -> {
-			if (e.getSelected().isEmpty()) {
-				editor.setVisible(false);
-			}
-			else {
-				editor.editCustomer((Customer) grid.getSelectedRow());
-			}
-		});
+        // Connect selected Customer to editor or hide if none is selected
+        grid.addSelectionListener(e -> {
+            if (e.getSelected().isEmpty()) {
+                editor.setVisible(false);
+            } else {
+                editor.editCustomer((Customer) grid.getSelectedRow());
+            }
+        });
 
-		// Instantiate and edit new Customer the new button is clicked
-		addNewBtn.addClickListener(e -> editor.editCustomer(new Customer("", "")));
+        // Instantiate and edit new Customer the new button is clicked
+        downloadButton.addClickListener(e -> editor.editCustomer(new Customer("", "")));
 
-		// Listen changes made by the editor, refresh data from backend
-		editor.setChangeHandler(() -> {
-			editor.setVisible(false);
-			listCustomers(filter.getValue());
-		});
+        // Listen changes made by the editor, refresh data from backend
+        editor.setChangeHandler(() -> {
+            editor.setVisible(false);
+            listCustomers(filter.getValue());
+        });
 
-		// Initialize listing
-		listCustomers(null);
-	}
+        // Initialize listing
+        listCustomers(null);
+    }
 
-	// tag::listCustomers[]
-	private void listCustomers(String text) {
-		if (StringUtils.isEmpty(text)) {
-			grid.setContainerDataSource(
-					new BeanItemContainer(Customer.class, repo.findAll()));
-		}
-		else {
-			grid.setContainerDataSource(new BeanItemContainer(Customer.class,
-					repo.findByLastNameStartsWithIgnoreCase(text)));
-		}
-	}
-	// end::listCustomers[]
+    // tag::listCustomers[]
+    private void listCustomers(String text) {
+        if (StringUtils.isEmpty(text)) {
+            grid.setContainerDataSource(
+                    new BeanItemContainer(Customer.class, repo.findAll()));
+        } else {
+            grid.setContainerDataSource(new BeanItemContainer(Customer.class,
+                    repo.findByLastNameStartsWithIgnoreCase(text)));
+        }
+    }
+    // end::listCustomers[]
 
 }
