792a9f5c3c313f54b9fe7e4c11d4876f79938a1c
tdang <dangtritue@gmail.com>
2016-06-29 17:30:15 +0700

    app launcher

---
+++ musicdownloader-web/src/main/java/com/media/browser/MyApp.java
@@ -0,0 +1,28 @@
+package com.media.browser;
+
+import javax.swing.*;
+
+public class MyApp {
+    public static void main(String[] args) {
+        String appUrl = "http://localhost:8080";
+        if (java.awt.Desktop.getDesktop() != null) {
+            JFrame frame = new JFrame();
+            JPanel panel = new JPanel();
+            final JTextField urlField = new JTextField(appUrl);
+            JButton webButton = new JButton("Launch download manager");
+            webButton.addActionListener(e -> BareBonesBrowserLaunch.openURL(urlField.getText().trim()));
+            frame.setTitle("Musicdownloader Browser Launch");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            panel.add(new JLabel("URL:"));
+            panel.add(urlField);
+            panel.add(webButton);
+            frame.getContentPane().add(panel);
+            frame.pack();
+            frame.setVisible(true);
+        } else {
+            BareBonesBrowserLaunch.openURL(appUrl);
+        }
+
+    }
+
+}
--- musicdownloader-web/src/main/java/hello/Application.java
+++ musicdownloader-web/src/main/java/hello/Application.java
@@ -1,70 +1,56 @@
 package hello;
 
-import edu.stanford.ejalbert.BrowserLauncher;
+import com.media.browser.MyApp;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.Bean;
-
-import java.awt.*;
-import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.URL;
 
 @SpringBootApplication
 public class Application {
 
-	private static final Logger log = LoggerFactory.getLogger(Application.class);
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class);
-	}
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(Application.class).headless(false).run(args);
+    }
 
-	@Bean
-	public CommandLineRunner loadData(CustomerRepository repository) {
-		return (args) -> {
-			// save a couple of customers
-			repository.save(new Customer("Jack", "Bauer"));
-			repository.save(new Customer("Chloe", "O'Brian"));
-			repository.save(new Customer("Kim", "Bauer"));
-			repository.save(new Customer("David", "Palmer"));
-			repository.save(new Customer("Michelle", "Dessler"));
+    @Bean
+    public CommandLineRunner loadData(CustomerRepository repository) {
+        return (args) -> {
+            // save a couple of customers
+            repository.save(new Customer("Jack", "Bauer"));
+            repository.save(new Customer("Chloe", "O'Brian"));
+            repository.save(new Customer("Kim", "Bauer"));
+            repository.save(new Customer("David", "Palmer"));
+            repository.save(new Customer("Michelle", "Dessler"));
 
-			// fetch all customers
-			log.info("Customers found with findAll():");
-			log.info("-------------------------------");
-			for (Customer customer : repository.findAll()) {
-				log.info(customer.toString());
-			}
-			log.info("");
+            // fetch all customers
+            log.info("Customers found with findAll():");
+            log.info("-------------------------------");
+            for (Customer customer : repository.findAll()) {
+                log.info(customer.toString());
+            }
+            log.info("");
 
-			// fetch an individual customer by ID
-			Customer customer = repository.findOne(1L);
-			log.info("Customer found with findOne(1L):");
-			log.info("--------------------------------");
-			log.info(customer.toString());
-			log.info("");
+            // fetch an individual customer by ID
+            Customer customer = repository.findOne(1L);
+            log.info("Customer found with findOne(1L):");
+            log.info("--------------------------------");
+            log.info(customer.toString());
+            log.info("");
 
-			// fetch customers by last name
-			log.info("Customer found with findByLastNameStartsWithIgnoreCase('Bauer'):");
-			log.info("--------------------------------------------");
-			for (Customer bauer : repository
-					.findByLastNameStartsWithIgnoreCase("Bauer")) {
-				log.info(bauer.toString());
-			}
-			log.info("");
-			try {
-				String url = "http://localhost:8080";
-				BrowserLauncher browserLauncher = new BrowserLauncher();
-				browserLauncher.openURLinBrowser(url);
-			}catch (Exception e) {
-
-			}
-
-		};
-	}
+            // fetch customers by last name
+            log.info("Customer found with findByLastNameStartsWithIgnoreCase('Bauer'):");
+            log.info("--------------------------------------------");
+            for (Customer bauer : repository
+                    .findByLastNameStartsWithIgnoreCase("Bauer")) {
+                log.info(bauer.toString());
+            }
+            log.info("");
+            MyApp.main(null);
+        };
+    }
 }
